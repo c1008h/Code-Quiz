293f287b13a19759a652710fbcbbf3cb9d50c502
Christina Hong <102135325+c1008h@users.noreply.github.com>
2022-05-27 01:28:32 -0700

    Update script.js
    Timer starts and ends when quiz is over but time jumps for no reason. Initial input and saving score was saved to localStorage. Still trying to get item from local storage to display on high score screen.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-var timerElement = document.querySelector('#timer-count');
-var timer;
-var secondsLeft = 60; // Seconds Left on timer
-var storeInitials = []; // For user to enter initials
 var questionare = [
     {
         question: "Commonly used data types DO NOT include: ",
@@ -28,25 +24,29 @@ var questionare = [
         answers: ["a. JavaScript", "b. terminal/bash", "c. for loops", "d. console.log"],
         correctAnswer: "d. console.log"
     }
-]
-        
+]      
 var startScreen = document.querySelector('#startscreen');
 var startButton = document.querySelector('#start-button')
 var gameScreen = document.querySelector('#quiz-container')
 var displayQuestion = document.querySelector('#question');
 var choicesEl = document.querySelector('#choices')
 var result = document.querySelector('#display-results')
+
 var viewHigh = document.querySelector('#View_Highscores')
 var endScreen = document.querySelector('#endscreen')
 var highScorePage = document.querySelector('#highscore')
 var clickHighScores = document.querySelector('.clickHigh')
 
+var timeEl = document.querySelector('#timer-count');
+var secondsLeft = 60; // Seconds Left on timer
+var progress = 0
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
+    setTime();
     startScreen.remove()
-
+    
     function question1() {
         displayQuestion.textContent = questionare[0].question
 
@@ -75,10 +75,8 @@ function startGame() {
             } else {
                 wrongAnswer();
             }
-            
         })
     }
-
     question1();
 
     function question2(){
@@ -109,7 +107,6 @@ function startGame() {
                 wrongAnswer();
             }
         })
-        
     }
 
     function question3(){
@@ -139,9 +136,7 @@ function startGame() {
             } else {
                 wrongAnswer();
             }
-            
         })
-        
     }
 
     function question4(){
@@ -171,9 +166,7 @@ function startGame() {
             } else {
                 wrongAnswer();
             }
-            
         })
-        
     }
 
     function question5(){
@@ -198,21 +191,107 @@ function startGame() {
                     }
                 }
                 removeChildElement()
-                question5()
+                
             } else {
                 wrongAnswer();
             }
-            
         })
-        
     }
-
 }
-
 function correctA(){
     result.textContent = "Correct!"
+    progress++
 }
 function wrongAnswer(){
     result.textContent = "Wrong!"
-    //secondsLeft -= 5; 
+    secondsLeft -= 5; 
+}
+function endScreenDisplay() {
+    gameScreen.remove()
+    result.remove()
+    var h3El = document.getElementById('allDone')
+    var h5El = document.getElementById('final-score-is')
+    var spanEl = document.getElementById('score-result')
+    var initialFormInput = document.createElement('input')
+    var submitButton = document.createElement('button')
+
+    initialFormInput.setAttribute('class', 'form-control')
+    initialFormInput.setAttribute('type', 'text')
+    initialFormInput.setAttribute('placeholder', 'Type Your Initials')
+    initialFormInput.setAttribute('id', 'initials-form-input')
+    submitButton.setAttribute('type', 'button')
+    submitButton.setAttribute('class', 'btn btn-secondary')
+    submitButton.textContent = 'Save'
+
+    h3El.textContent = 'All done!'
+    h5El.textContent = 'Your final score is: '
+
+    endScreen.appendChild(initialFormInput)
+    endScreen.appendChild(submitButton)
+    spanEl.textContent = secondsLeft
+
+    submitButton.addEventListener('click', submitInitial)
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " second(s) left.";
+
+        if(secondsLeft === 0 || progress == 5) {
+            clearInterval(timerInterval);
+            endScreenDisplay();
+        }
+    }, 1000);
+}
+
+function submitInitial() {
+    var userInput = document.querySelector('input').value;
+        if(document.getElementById('initials-form-input').value.length == 0) {
+            alert('Please input initials');
+        } else {
+            var initials = userInput
+            var score = secondsLeft
+            localStorage.setItem(initials, score)
+            highScoreDisplay()
+        }
+}
+
+function highScoreDisplay() {
+    endScreen.remove();
+
+    var tableEl = document.createElement('table')
+    var theadEl = document.createElement('thead')
+    var trEl = document.createElement('tr')
+    trEl.setAttribute('class', 'col-12')
+    var headScore = document.createElement('th')
+    var headInitials = document.createElement('th')
+    headScore.setAttribute('scope', 'col')
+    headInitials.setAttribute('scope', 'col')
+    headScore.textContent = 'Score: '
+    headInitials.textContent = 'Initials: '
+    
+    highScorePage.appendChild(tableEl)
+    highScorePage.appendChild(theadEl)
+    highScorePage.appendChild(trEl)
+    highScorePage.appendChild(headInitials)
+    highScorePage.appendChild(headScore)
+
+    var tbodyEl = document.createElement('tbody')
+
+    for(i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.getItem(localStorage.key(i)))
+        var trBodyEl = document.createElement('tr')
+        var tdInitials = document.createElement('td')
+        var tdScore = document.createElement('td')
+        
+        //localStorage.getItem(initials).value = tdInitials[i].textContent
+        //tdInitials.textContent = localStorage.getItem(initials)
+        //tdScore.textContent = localStorage.getItem(score)
+
+        tbodyEl.appendChild(trBodyEl)
+        tdInitials.appendChild(tbodyEl)
+        tdScore.appendChild(tbodyEl)
+
+    }
 }
